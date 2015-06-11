9b1215b552a44a6b27972c0fdb4fa69f11798d29
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-06-11 13:07:53 +0200

    Remove not needed resources

--- sample/src/main/java/com/karumi/expandableselector/sample/MainActivity.java
+++ sample/src/main/java/com/karumi/expandableselector/sample/MainActivity.java
@@ -3,7 +3,7 @@ package com.karumi.expandableselector.sample;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivityActivity extends Activity {
+public class MainActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
