1ff8e1621f74bdad8a7f24bfd8ebc69948a58ff9
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-06-11 18:51:17 +0200

    Add test animation. We need to resize the view to be able to perform the animation

--- expandableselector/src/main/java/com/karumi/expandableselector/ExpandableSelector.java
+++ expandableselector/src/main/java/com/karumi/expandableselector/ExpandableSelector.java
@@ -1,14 +1,18 @@
 package com.karumi.expandableselector;
 
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,8 +21,11 @@ import java.util.List;
  */
 public class ExpandableSelector extends FrameLayout {
 
-  private final AttributeSet attrs;
+  private static final String Y_ANIMATION = "translationY";
+  
   private List<ExpandableItem> expandableItems = Collections.EMPTY_LIST;
+  private List<View> buttons = new LinkedList<View>();
+  private boolean isCollapsed = true;
 
   public ExpandableSelector(Context context) {
     this(context, null);
@@ -30,22 +37,19 @@ public class ExpandableSelector extends FrameLayout {
 
   public ExpandableSelector(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    this.attrs = attrs;
   }
 
   @TargetApi(Build.VERSION_CODES.LOLLIPOP)
   public ExpandableSelector(Context context, AttributeSet attrs, int defStyleAttr,
       int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
-    this.attrs = attrs;
   }
 
   /**
    * Configures a List<ExpandableItem> to be shown. By default, the list of ExpandableItems is
    * going to be shown collapsed. Please take into account that this method creates ImageButtons
    * based on the size of the list passed as parameter. Don't use this library as a RecyclerView
-   * and
-   * take into account the number of elements to show.
+   * and take into account the number of elements to show.
    */
   public void setExpandableItems(List<ExpandableItem> expandableItems) {
     validateExpandableItems(expandableItems);
@@ -53,13 +57,36 @@ public class ExpandableSelector extends FrameLayout {
     renderItems();
   }
 
+  public void expand() {
+    int itemPosition = 0;
+    View button = buttons.get(0);
+    ObjectAnimator.ofFloat(button, Y_ANIMATION, -100).start();
+  }
+
+  public void collapse() {
+    int itemPosition = 0;
+    Animation animation = new TranslateAnimation(0, 0, 0, 100);
+    buttons.get(0).startAnimation(animation);
+  }
+
+  @Override public boolean onTouchEvent(MotionEvent event) {
+    if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+      if (isCollapsed) {
+        expand();
+      } else {
+        collapse();
+      }
+    }
+    return true;
+  }
+
   private void renderItems() {
     int numberOfItems = expandableItems.size() - 1;
     LayoutInflater inflater = LayoutInflater.from(getContext());
     for (int i = numberOfItems; i >= 0; i--) {
       View button = inflater.inflate(R.layout.expandable_item, this, false);
-      //View button = new ImageButton(getContext(), attrs, R.attr.expandableItemStyle);
       addView(button);
+      buttons.add(button);
     }
   }
 
