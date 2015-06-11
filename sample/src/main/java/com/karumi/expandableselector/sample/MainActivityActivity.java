ca2d068b1fcc406deee8bc640660455d1ca40b64
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-06-11 12:59:41 +0200

    Add library module to the project

---
+++ sample/src/main/java/com/karumi/expandableselector/sample/MainActivityActivity.java
@@ -0,0 +1,12 @@
+package com.karumi.expandableselector.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_activity);
+  }
+}
